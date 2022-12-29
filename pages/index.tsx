b389935ddf07fded3e58a32e43081996c109e6e7
Adrian Rocke <me@adrianrocke.com>
2022-12-29 16:34:14 -0600

    chore: clean up index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import VerseTyper from '../components/VerseTyper'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <VerseTyper className={styles.input} text="To you, O Lord, I lift up my soul." />
-      </main>
     </div>
   )
 }
