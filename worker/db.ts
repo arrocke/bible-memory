adf3ce46e25458552b16e96d05362bf8fc28a47c
Adrian Rocke <me@adrianrocke.com>
2023-04-11 17:05:30 -0500

    refactor: autoincrement passage key

--- worker/db.ts
+++ worker/db.ts
@@ -1,47 +1,63 @@
-import { openDB } from 'idb';
+import { DBSchema, openDB } from "idb";
 
 export interface DbPassage {
-  id: string
-  reference: string
-  text: string
-  level: number
-  reviewDate?: Date
+  id: number;
+  reference: string;
+  text: string;
+  level: number;
+  reviewDate?: Date;
 }
 
-interface DbSchema {
+interface DbSchema extends DBSchema {
   passages: {
-    key: string
-    value: DbPassage
-  }
+    key: number;
+    value: DbPassage;
+  };
 }
 
 export function open() {
-  const dbPromise = openDB<DbSchema>('bible-memory', 1, {
-    upgrade(db) {
-      db.createObjectStore('passages');
+  let migratePassages = false;
+  const dbPromise = openDB<DbSchema>("bible-memory", 2, {
+    upgrade(db, prevVersion, newVersion, transaction) {
+      if (prevVersion < 2) {
+        migratePassages = true;
+        transaction.objectStore("passages").name = "passages-old";
+      }
+      db.createObjectStore("passages", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
     },
-  })
+  });
+
+  dbPromise.then(async (db) => {
+    if (migratePassages) {
+      const passages = await db.getAll("passages-old" as any);
+
+      for (const { id, ...passage } of passages) {
+        await db.put("passages", passage);
+      }
+    }
+  });
 
   return {
     passages: {
-      async insert(data: Omit<DbPassage, 'id'>): Promise<DbPassage> {
-        const id = (1 + await (await dbPromise).count('passages')).toString()
-        const passage = { ...data, id }
-        await (await dbPromise).put('passages', passage, id)
-        return passage
+      async insert(data: Omit<DbPassage, "id">): Promise<DbPassage> {
+        const id = await (await dbPromise).put("passages", data as DbPassage);
+        return { ...data, id };
       },
       async update(passage: DbPassage): Promise<void> {
-        await (await dbPromise).put('passages', passage, passage.id)
+        await (await dbPromise).put("passages", passage, passage.id);
       },
-      async getById(id: string): Promise<DbPassage> {
-        return await (await dbPromise).get('passages', id)
+      async getById(id: number): Promise<DbPassage | undefined> {
+        return await (await dbPromise).get("passages", id);
       },
       async getAll(): Promise<DbPassage[]> {
-        return await (await dbPromise).getAll('passages')
+        return await (await dbPromise).getAll("passages");
       },
-      async delete(id: string): Promise<void> {
-        return await (await dbPromise).delete('passages', id)
-      }
-    }
-  }
+      async delete(id: number): Promise<void> {
+        return await (await dbPromise).delete("passages", id);
+      },
+    },
+  };
 }
