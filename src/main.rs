7ef3725317174783da647a3686674d0e58ea0f42
Adrian Rocke <me@adrianrocke.com>
2024-01-07 15:43:08 -0600

    chore: replace handlebars with askama

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,61 @@
 use std::net::Ipv4Addr;
 
+use askama::Template;
 use axum::{
     extract::{FromRef, Path, State},
     response::IntoResponse,
     routing::get,
     serve, Router,
 };
-use axum_template::{engine::Engine, Key, RenderHtml};
-use handlebars::Handlebars;
-use serde::Serialize;
 use tokio::net::TcpListener;
 
-// Type alias for our engine. For this example, we are using Handlebars
-type AppEngine = Engine<Handlebars<'static>>;
-
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Clone)]
 pub struct Passage {
     id: u32,
     reference: String,
     level: u32,
-    // review_date: std::time::Date
 }
 
-#[derive(Debug, Serialize)]
-struct IndexData {
-    title: String,
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
     passages: Vec<Passage>,
 }
 
-async fn get_index(
-    engine: AppEngine,
-    State(passages): State<Vec<Passage>>,
-    Key(key): Key,
-) -> impl IntoResponse {
-    RenderHtml(
-        key,
-        engine,
-        IndexData {
-            title: String::from("Bible Memory"),
-            passages,
-        },
-    )
+async fn get_index(State(passages): State<Vec<Passage>>) -> impl IntoResponse {
+    IndexTemplate { passages }
 }
 
-#[derive(Debug, Serialize)]
-struct ReviewData {
-    title: String,
+#[derive(Template)]
+#[template(path = "review.html")]
+struct ReviewTemplate {
     passage: Passage,
 }
 
 async fn get_review(
-    engine: AppEngine,
     State(passages): State<Vec<Passage>>,
-    Key(key): Key,
     Path(resource_id): Path<u32>,
 ) -> impl IntoResponse {
     let Some(passage) = passages.iter().find(|&p| p.id == resource_id) else {
         panic!("Not found")
     };
-    RenderHtml(
-        key,
-        engine,
-        ReviewData {
-            title: format!("Review {} | Bible Memory", passage.reference),
-            passage: passage.clone(),
-        },
-    )
+    ReviewTemplate {
+        passage: passage.clone(),
+    }
 }
 
 #[derive(Clone, FromRef)]
 struct AppState {
-    engine: AppEngine,
     passages: Vec<Passage>,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut hbs = Handlebars::new();
-    hbs.register_template_file("head", "./src/templates/partials/head.hbs")
-        .unwrap();
-    hbs.register_template_file("body", "./src/templates/partials/body.hbs")
-        .unwrap();
-    hbs.register_template_file("/", "./src/templates/pages/index.hbs")
-        .unwrap();
-    hbs.register_template_file("/:passage_id/review", "./src/templates/pages/review.hbs")
-        .unwrap();
-
     let app = Router::new()
         .route("/", get(get_index))
         .route("/:passage_id/review", get(get_review))
         // Create the application state
         .with_state(AppState {
-            engine: Engine::from(hbs),
             passages: Vec::from([
                 Passage {
                     id: 1,
